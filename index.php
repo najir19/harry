0d9fa8b64851b4fc059691473690f0a728f3b2c1
najir19 <najirahamed29@gmail.com>
2024-12-14 17:51:36 +0600

    Single blog started

--- index.php
+++ index.php
@@ -16,6 +16,9 @@ get_header();
                             <?php get_template_part('template-parts/content', get_post_format()); ?>
                         <?php endwhile ?>
                     <?php endif ?>
+                    <div class="tp-pagination tp-pagination-style-2 mt-20">
+                        <?php harry_navigation(); ?>
+                    </div>
                 </div>
             </div>
             <div class="col-xxl-4 col-lg-4">
--- template-parts/content.php
+++ template-parts/content.php
@@ -1,22 +1,96 @@
 <article id="post-<?php the_ID(); ?>" <?php post_class('tp-format-standard postbox__item format-standard mb-50 transition-3'); ?>>
-    <?php if (has_post_thumbnail()): ?>
-        <div class="postbox__thumb w-img">
-            <a href="<?php the_permalink(); ?>">
-                <?php the_post_thumbnail(); ?>
+    <?php if (is_single()): ?>
+        <div class="postbox__thumb w-img mb-30">
+            <a href="blog-details.html">
+                <img src="assets/img/blog/blog-big-1.jpg" alt="">
             </a>
         </div>
-    <?php endif; ?>
-    <div class="postbox__content">
-        <!-- blog meta -->
-        <?php get_template_part('template-parts/blog/post-meta'); ?>
-        <h3 class="postbox__title">
-            <a href="<?php the_permalink(); ?>"><?php the_title(); ?></a>
-        </h3>
-        <div class="postbox__text">
-            <?php the_excerpt(); ?>
+        <div class="postbox__meta">
+            <span>
+                <a href="#">
+                    <svg width="13" height="14" viewBox="0 0 13 14" fill="none" xmlns="http://www.w3.org/2000/svg">
+                        <path d="M11.6667 13V11.6667C11.6667 10.9594 11.3857 10.2811 10.8856 9.78105C10.3855 9.28095 9.70724 9 9 9H3.66667C2.95942 9 2.28115 9.28095 1.78105 9.78105C1.28095 10.2811 1 10.9594 1 11.6667V13" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" />
+                        <path d="M6.33317 6.33333C7.80593 6.33333 8.99984 5.13943 8.99984 3.66667C8.99984 2.19391 7.80593 1 6.33317 1C4.86041 1 3.6665 2.19391 3.6665 3.66667C3.6665 5.13943 4.86041 6.33333 6.33317 6.33333Z" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" />
+                    </svg>
+                    Shahnewaz
+                </a>
+            </span>
+            <span>
+                <svg width="15" height="15" viewBox="0 0 15 15" fill="none" xmlns="http://www.w3.org/2000/svg">
+                    <path d="M7.5 14C11.0899 14 14 11.0899 14 7.5C14 3.91015 11.0899 1 7.5 1C3.91015 1 1 3.91015 1 7.5C1 11.0899 3.91015 14 7.5 14Z" stroke="currentColor" stroke-width="1.5" stroke-linecap="round" stroke-linejoin="round" />
+                    <path d="M7.5 3.59961V7.49961L10.1 8.79961" stroke="currentColor" stroke-width="1.5" stroke-linecap="round" stroke-linejoin="round" />
+                </svg> January 22, 2022
+            </span>
+            <span>
+                <svg width="14" height="14" viewBox="0 0 14 14" fill="none" xmlns="http://www.w3.org/2000/svg">
+                    <path d="M12.9718 6.66668C12.9741 7.54659 12.769 8.4146 12.3732 9.20001C11.9039 10.1412 11.1825 10.9328 10.2897 11.4862C9.39697 12.0396 8.36813 12.3329 7.31844 12.3333C6.4406 12.3356 5.57463 12.13 4.79106 11.7333L1 13L2.26369 9.20001C1.86791 8.4146 1.66281 7.54659 1.6651 6.66668C1.66551 5.61452 1.95815 4.58325 2.51025 3.68838C3.06236 2.79352 3.85211 2.0704 4.79106 1.60002C5.57463 1.20331 6.4406 0.997725 7.31844 1.00002H7.65099C9.03729 1.07668 10.3467 1.66319 11.3284 2.64726C12.3102 3.63132 12.8953 4.94378 12.9718 6.33334V6.66668Z" stroke="currentColor" stroke-width="1.5" stroke-linecap="round" stroke-linejoin="round" />
+                </svg>35
+            </span>
         </div>
-        <div class="postbox__read-more">
-            <a href="<?php the_permalink(); ?>" class="tp-btn"><?php echo esc_html("read more") ?></a>
+        <div class="postbox__details-content-wrapper">
+            <h3 class="postbox__details-title">As influential as runways are, trends not solely <br> born them. There was a clear direction coming their social media platform.</h3>
+            <p>One morning, when Gregor Samsa woke from troubled dreams, he found himself transformed in his bed into a horrible vermin. He lay on his armour-like back, and if he lifted his head a little he could see his brown belly, slightly domed and divided by arches into stiff sections. The bedding was hardly able to cover it and seemed ready to slide off any moment.</p>
+            <p>I should be incapable of drawing a single stroke at the present moment; and yet I feel that never was a greater artist than now.</p>
+
+            <div class="postbox__img m-img mt-50 mb-45">
+                <img src="assets/img/blog/details/blog-details-1.jpg" alt="">
+                <h5 class="postbox__img-caption">Image by <a href="#">@example01</a></h5>
+            </div>
+
+            <p>His many legs, pitifully thin compared with the size of the rest of him, waved about helplessly as he looked. "What's happened to me?" he thought. It wasn't a dream. His room, a proper human room although a little too small, lay peacefully between its four familiar walls. A collection of textile samples lay spread out on the table - Samsa was a travelling salesman - and above it there hung a picture that he had recently cut out of an illustrated magazine.</p>
+
+            <div class="postbox__quote">
+                <blockquote>
+                    <div class="postbox__quote-icon">
+                        <span>
+                            <svg width="34" height="28" viewBox="0 0 34 28" fill="none" xmlns="http://www.w3.org/2000/svg">
+                                <path d="M25.6645 0C27.2731 0 28.5892 0.329033 29.6129 0.987098C30.6366 1.64516 31.4774 2.48602 32.1355 3.50968C32.9398 4.67957 33.4516 5.95915 33.671 7.34839C33.8903 8.73764 34 9.87097 34 10.7484C34 14.3312 33.086 17.585 31.2581 20.5097C29.4301 23.4344 26.5785 25.8108 22.7032 27.6387L21.7161 25.6645C23.9828 24.714 25.9204 23.2151 27.529 21.1677C29.2108 19.1204 30.0516 17.0366 30.0516 14.9161C30.0516 14.0387 29.9419 13.271 29.7226 12.6129C28.5527 13.5634 27.2 14.0387 25.6645 14.0387C23.7634 14.0387 22.1183 13.4172 20.729 12.1742C19.3398 10.9312 18.6452 9.21291 18.6452 7.01936C18.6452 4.97205 19.3398 3.29033 20.729 1.9742C22.1183 0.658065 23.7634 0 25.6645 0ZM7.01936 0C8.62796 0 9.94409 0.329033 10.9677 0.987098C11.9914 1.64516 12.8323 2.48602 13.4903 3.50968C14.2946 4.67957 14.8065 5.95915 15.0258 7.34839C15.2452 8.73764 15.3548 9.87097 15.3548 10.7484C15.3548 14.3312 14.4409 17.585 12.6129 20.5097C10.7849 23.4344 7.93333 25.8108 4.05806 27.6387L3.07097 25.6645C5.33763 24.714 7.27527 23.2151 8.88387 21.1677C10.5656 19.1204 11.4065 17.0366 11.4065 14.9161C11.4065 14.0387 11.2968 13.271 11.0774 12.6129C9.90753 13.5634 8.55484 14.0387 7.01936 14.0387C5.11828 14.0387 3.47312 13.4172 2.08387 12.1742C0.694624 10.9312 0 9.21291 0 7.01936C0 4.97205 0.694624 3.29033 2.08387 1.9742C3.47312 0.658065 5.11828 0 7.01936 0Z" fill="currentColor" />
+                            </svg>
+                        </span>
+                    </div>
+                    <p>“I try as much as possible to give you a great basic product and what comes out, I feel, is really amazing.”</p>
+                </blockquote>
+            </div>
+
+            <div class="postbox__features">
+                <h4 class="postbox__features-title">Learning from failure</h4>
+                <p>The European languages are members of the same family. Their separate existence is a myth. For science europe uses the same vocabulary. </p>
+
+                <ul>
+                    <li>Get yourself comfortable.</li>
+                    <li>Manage your workspace and organize your desk.</li>
+                    <li>Keep In touch with your co-workers.</li>
+                </ul>
+            </div>
+            <p>The new common language will be more simple and regular than the existing European languages. It will be as simple as Occidental; in fact, it will be Occidental. To an English person, it will seem like simplified English, as a skeptical Cambridge friend of mine told me what Occidental is. The European languages are members of the same family.</p>
+
         </div>
-    </div>
+
 </article>
+
+<?php else : ?>
+
+    <article id="post-<?php the_ID(); ?>" <?php post_class('tp-format-standard postbox__item format-standard mb-50 transition-3'); ?>>
+        <?php if (has_post_thumbnail()): ?>
+            <div class="postbox__thumb w-img">
+                <a href="<?php the_permalink(); ?>">
+                    <?php the_post_thumbnail(); ?>
+                </a>
+            </div>
+        <?php endif; ?>
+        <div class="postbox__content">
+            <!-- blog meta -->
+            <?php get_template_part('template-parts/blog/post-meta'); ?>
+            <h3 class="postbox__title">
+                <a href="<?php the_permalink(); ?>"><?php the_title(); ?></a>
+            </h3>
+            <div class="postbox__text">
+                <?php the_excerpt(); ?>
+            </div>
+            <div class="postbox__read-more">
+                <a href="<?php the_permalink(); ?>" class="tp-btn"><?php echo esc_html("read more") ?></a>
+            </div>
+        </div>
+    </article>
+
+<?php endif ?>
